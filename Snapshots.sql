b02cba3331b3105824d4c9f0fd7a00f10d3f6b8d
Arpita Saha <67737156+arpitabksaha@users.noreply.github.com>
2020-10-09 16:17:47 +0530

    Add files via upload

---
+++ Snapshots.sql
@@ -0,0 +1,17 @@
+CREATE SNAPSHOT system.tableorganiser1 AS 
+    SELECT * FROM system.tableorganiser ;
+    
+CREATE SNAPSHOT system.tablereseller1 AS 
+    SELECT * FROM system.tablereseller ;
+    
+CREATE SNAPSHOT system.tablecustomers1 AS 
+    SELECT * FROM system.tablecustomers ;
+    
+CREATE SNAPSHOT system.tableevent1 AS 
+    SELECT * FROM system.tableevent ;
+    
+CREATE SNAPSHOT system.tabletickets1 AS 
+    SELECT * FROM system.tabletickets ; 
+    
+CREATE SNAPSHOT system.torganiserreseller1 AS 
+    SELECT * FROM system.torganiserreseller ; 
